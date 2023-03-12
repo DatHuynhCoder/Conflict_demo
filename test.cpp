450876170b5f3788b8caa49aece190d4f3011f14
Huynh Ngoc Tan <18130210@st.hcmuaf.edu.vn>
2023-03-12 10:12:57 +0700

    Change main method

--- test.cpp
+++ test.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(){
     int a,b;
     cin>>a>>b;
-    cout<<"Tong: "<<a-b;
-    cout<<"Hieu: "<<a+b;
+    cout<<"Tong: "<<a+b;
+    cout<<"Hieu: "<<a-b;
     cout<<"Hom nay la thu 7";
     return 0;
 }
