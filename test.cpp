550a094aab3b170c161d97a8bd6aadc731c3e22c
DatHuynhCoder <htdat2004@gmail.com>
2023-03-12 10:08:56 +0700

    creat test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"Tong: "<<a-b;
+    cout<<"Hieu: "<<a+b;
+    cout<<"Hom nay la thu 7";
+    return 0;
+}
